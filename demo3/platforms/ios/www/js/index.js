85ffada248c18ce73f11974eeeda522d592204a7
blmiller <brian@Cassandras-MacBook-Pro-2.local>
2014-04-13 12:28:19 -0400

    Got GeoLocation working in IOS.  For some reason it's not showing up properly in Android though.  Not sure what that's about.
    Signed-off-by: blmiller <brian@Cassandras-MacBook-Pro-2.local>

--- demo3/platforms/ios/www/js/index.js
+++ demo3/platforms/ios/www/js/index.js
@@ -53,121 +53,60 @@ var onDeviceReady = function () {
    }),
     
    $("#device").on('click', function(){
-                    
                     var element = document.getElementById('deviceProperties');
-                    
+            
                     element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
-                    
                     'Device Cordova: '  + device.cordova  + '<br />' +
-                    
                     'Device Platform: ' + device.platform + '<br />' +
-                    
                     'Device UUID: '     + device.uuid     + '<br />' +
-                    
                     'Device Version: '  + device.version  + '<br />';
-                    
-                   });
+                   }),
     
     
-   /* $("#loc-pg").on('click', function () {
+   $("#geoFindMe").on('click', function () {
                        
-                       var output = document.getElementById('outloc');
+     var output = document.getElementById('outloc');
                        
-                       if (!navigator.geolocation){
-                       
-                       alert("whoops I failed");
-                       
-                       output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-                       
-                       return;
-                       
-                       }
-                        
-                        function successloc(position) {
-                        
-                        alert("getting location");
-                        
-                        var latitude  = position.coords.latitude;
-                        
-                        var longitude = position.coords.longitude;
-                        
-                        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-                        
-                        var img = new Image();
-                        
-                        img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-                        
-                        output.appendChild(img);
-                        
-                        };
-                        
-                        function errorloc() {
-                        
-                        alert("got an error");
-                        
-                        output.innerHTML = "Unable to retrieve your location";
-                        
-                        };
-                       
-                       
-                       output.innerHTML = "O hi!!";
-                       
-                       navigator.geolocation.getCurrentPosition(successloc, errorloc);
-                       
-                       alert("here!!!");
-                       
-                       });
-    
-    // onSuccess Geolocation
-    
-     
-     var onSuccess = function () {
-     
-     alert('success');
-     
-     $("#loc-pg").on('click', function(){
-     
-     var element = document.getElementById('geolocation');
-     
-     element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-     
-     'Longitude: '          + position.coords.longitude             + '<br />' +
-     
-     'Altitude: '           + position.coords.altitude              + '<br />' +
-     
-     'Accuracy: '           + position.coords.accuracy              + '<br />' +
-     
-     'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
-     
-     'Heading: '            + position.coords.heading               + '<br />' +
-     
-     'Speed: '              + position.coords.speed                 + '<br />' +
-     
-     'Timestamp: '          +                                   position.timestamp          + '<br />';
-     
-     });
-     
-     
-     
-     // onError Callback receives a PositionError object
-     
-     
-     
-     function onError(error) {
-     
-         alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-     
+     if (!navigator.geolocation){
+       alert("Geolocation not supported");
+       output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+       return;
      }
-     
-     
-     
+                      
+     function successloc(position) {
+       var latitude  = position.coords.latitude;
+       var longitude = position.coords.longitude;
+       
+       output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+       var img = new Image();
+                        
+       img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+       output.appendChild(img);
      };
-    
-    $('#instagram-pg').on('pageinit', function(){
-     
-     });*/
-    
-    
+                        
+     function errorloc(error) {
+
+       switch(error.code) {
+         case error.PERMISSION_DENIED:
+            output.innerHTML = "User denied the request for Geolocation."
+            break;
+          case error.POSITION_UNAVAILABLE:
+            output.innerHTML = "Location information is unavailable."
+            break;
+          case error.TIMEOUT:
+            output.innerHTML = "The request to get user location timed out."
+            break;
+          case error.UNKNOWN_ERROR:
+            output.innerHTML = "An unknown error occurred."
+            break;
+          default :
+            output.innerHTML = "Unhandled error retrieving location";
+            break;
+       }
+     };
+     navigator.geolocation.getCurrentPosition(successloc, errorloc);
+                       
+   });
 };
 
 document.addEventListener("deviceready", onDeviceReady, false);
