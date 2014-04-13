efb90acec24c17f65c5c17c6e711beb92e097e72
blmiller <brian@Cassandras-MacBook-Pro-2.local>
2014-04-13 11:31:07 -0400

    Cleaned up the javascript for Instagram.  The layout is still pretty ugly, but it's now displaying the images as well as the tags and comments for each image.
    Signed-off-by: blmiller <brian@Cassandras-MacBook-Pro-2.local>

--- demo3/platforms/ios/www/js/index.js
+++ demo3/platforms/ios/www/js/index.js
@@ -1,38 +1,56 @@
 // Wait for device API libraries to load
-
 //
-
 // device APIs are available
 var onDeviceReady = function () {
-$.ajax( {
+    // Onclick handler for the instapicsButton.  Populates the insstagram pictures on the
+    // #insta-pg data page.
+    $("#instapicsButton").on('click',function() {
+       // Make the ajax call to instagram pulling recent images.
+       $.ajax( {
        type: "GET",
        dataType: "jsonp",
        cache: false,
        url: "https://api.instagram.com/v1/tags/poker/media/recent?client_id=5db8267c7c3240edaf6fbaafd02858b3",
        success: function(response) {
-       //         var fullComment = "X";
-       var theInnerHtml = "";
-       var myData = response.data;
-       var images;
-       //         var tags = [];
-       //         var comments = [];
+         //- This is the success function.  We're going to build out the unordered list
+         //  with an ID of #insta with the images, tags and comments that were returned
+         //  by the call.
+         var theInnerHtml = "";
+         var myData = response.data;
+         var image;
+         var tags = [];
+         var comments = [];
        
-       for(i in myData) {
-       images = myData[i].images.low_resolution.url;
-       theInnerHtml += "<img src='" + images + "'>";
-       //           tags = myData[i].tags;
-       //           comments = myData[i].comments.data;
-       //           for (j in comments) {
-       //             fullComment = fullComment + comments[j].text;
-       //           }
-       }
-       $("#insta").html(theInnerHtml);
+         // For each element in the data returned, grab the image, any tags and any comments
+         // and build the display.
+         for(i in myData) {
+           image = myData[i].images.low_resolution.url;
+              
+           // We have the image url, add it to the tag we're currently working on
+           theInnerHtml += "<li><img src='" + image + "' width = 25%>";
+              
+           // Now we've extracted the tags for the image, add them to the current tag
+           theInnerHtml += "<p>" + tags + "</p>";
+                            
+           // Next, we'll extract the comments associated with the image and add them
+           // to the current tag.
+           comments = myData[i].comments.data;
+           for (j in comments) {
+             theInnerHtml += "<p>" + comments[j].text + "</p>";
+           }
+           // And finally before getting the next image, close off the current tag.
+           theInnerHtml += "</li>"
+         }
+         $("#insta").html(theInnerHtml);
        },
        error: function(error) {
-       alert("YYY");
-       $("#insta").html("YYY");
+         var theInnerHtml = "";
+         theInnerHtml = "<p>An error occurred attempting to retrieve data from Instagram</p>";
+         theInnerHtml += "<p>Error:  " + error + "</p>"
+         $("#insta").html(theInnerHtml);
        }
-       }),
+     })
+   }),
     
    $("#device").on('click', function(){
                     
